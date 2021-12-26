b2786480312e29f8d6cce0f9557e1eb9b3cf403a
stefanelli990 <stefan.mohenski@gmail.com>
2021-12-26 03:35:17 +0100

    last change

---
+++ scripts/script.js
@@ -0,0 +1,6 @@
+const btn = document.querySelector('.btn');
+const btnArrow = document.querySelector('.btn-arrow');
+
+btn.addEventListener('mouseover', () => {
+    btnArrow.style.display = 'inline';
+});
